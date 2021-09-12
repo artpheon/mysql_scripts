7049b7335d4f568616b726ba2ac2a92571e1c2a0
Artur N <idcdtokms@gmail.com>
2021-09-12 15:59:35 +0300

    added conf.json, refactored the script

--- ticket_search.py
+++ ticket_search.py
@@ -2,41 +2,56 @@ from mysql.connector import Error
 from sqlalchemy import create_engine
 import pandas
 import sys
-
 import sqlalchemy
+import json
 
-def get_engine():
+
+# INSERT IGNORE
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.expression import Insert
+
+@compiles(Insert)
+def _prefix_insert_with_ignore(insert, compiler, **kw):
+    return compiler.visit_insert(insert.prefix_with('IGNORE'), **kw)
+
+def get_engine(data):
     # rename columns in excel - yes
-    user = input('Введите имя пользователя: ')
-    password = input('Введите пароль: ')
-    host = input('Введите имя хоста: ')
-    port = input('Введите порт: ')
+    user = data['user']
+    password = data['password']
+    host = data['host']
+    port = data['port']
     if len(user)==0 or len(password)==0 or len(host)==0 or port==0:
         raise SystemExit('Некоторые данные не были введены')
     engine = create_engine('mysql+mysqlconnector://{0}:{1}@{2}:{3}'.format(user, password, host, port))
     return engine
 
-def get_data_frame():
+def get_data_frame(data):
     filename = sys.argv[1]
     excel = pandas.read_excel('./'+filename, sheet_name=0)
     if excel.empty:
         return excel
-    excel.drop(['Дата первого ответа', 'ID компании', 'Customer Name', 'Дерево сообщений', 'FirstResponseInMin', 'FirstResponseDiffInMin'], axis=1, inplace=True)
-    excel.set_axis(['ticket_id', 'age', 'created_time', 'closed', 'date_first_block', 'state', 'priority', 'queue', 'to_block', 'owner', 'name', 'surname', 'sender', 'subject', 'spent_time', 'SolutionInMin', 'SolutionDiffInMin'], axis=1, inplace=True)
+    #excel.drop(['Дата первого ответа', 'ID компании', 'Customer Name', 'Дерево сообщений', 'FirstResponseInMin', 'FirstResponseDiffInMin'], axis=1, inplace=True)
+    excel.set_axis(['ticket_id', 'age', 'created', 'closed', 'first_block', 'first_repl', 'state', 'priority', 'queue', 'to_block', 'owner', 'first_name', 'last_name', 'company_id', 'customer_name', 'sender', 'subject', 'spent_time', 'message_tree', 'solution_in_min', 'solution_diff_min', 'first_response_in_min', 'first_response_diff_min'], axis=1, inplace=True)
     return excel
 
 try:
     if len(sys.argv) < 2:
         raise SystemExit("Имя файла должно быть аргументом к скрипту")
-    engine = get_engine()
-    df = get_data_frame()
+    # settings = pandas.read_json("conf.json")
+    with open('conf.json', 'r') as f:
+        data = json.load(f)
+    engine = get_engine(data)
+    df = get_data_frame(data)
     if df.empty:
         engine.dispose()
         raise SystemExit("Некорректное имя excel файла, либо нет доступа")
-    tname = input('Введите имя базы/схемы: ')
-    ttable = input('Введите имя таблицы: ')
-    df.to_sql(name=ttable, schema=tname, con=engine, index=False, if_exists='append')
-    print('Данные из файла {} были загружены в базу данных.'.format(sys.argv[1]))
+    tname = data['schema']
+    ttable = data['table']
+    try:
+        df.to_sql(name=ttable, schema=tname, con=engine, index=False, if_exists='append')
+    except ValueError as vx: print(vx)
+    except Exception as ex: print(ex)
+    else: print('Данные из файла {} были загружены в базу данных.'.format(sys.argv[1]))
 
 except Error as e:
     print("Возникла ошибка:", e)
