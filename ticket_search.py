724388ddc0f769af4c08c6642c23b6db79483612
hrobbin <idcdtokms@gmail.com>
2021-09-14 14:54:11 +0300

    finished with ticketsearch

--- ticket_search.py
+++ ticket_search.py
@@ -4,6 +4,7 @@ import pandas
 import sys
 import sqlalchemy
 import json
+import re
 
 
 # INSERT IGNORE
@@ -25,23 +26,34 @@ def get_engine(data):
     engine = create_engine('mysql+mysqlconnector://{0}:{1}@{2}:{3}'.format(user, password, host, port))
     return engine
 
-def get_data_frame(data):
+def match_time(line):
+    res = re.search(r"(\d+)\D+(\d+)\D+(\d+)", line)
+    if not res:
+        res = re.search(r"(\d+)\D+(\d+)", line)
+    if res.lastindex == 2:
+        val = res.group(1) + ":" + res.group(2) + ":00"
+    elif res.lastindex == 3:
+        val = str(int(res.group(1)) * 24 + int(res.group(2))) + ":" + res.group(3) + ":00"
+    else:
+        val = "00:00:00"
+    return val
+
+def get_data_frame():
     filename = sys.argv[1]
     excel = pandas.read_excel('./'+filename, sheet_name=0)
     if excel.empty:
         return excel
-    #excel.drop(['Дата первого ответа', 'ID компании', 'Customer Name', 'Дерево сообщений', 'FirstResponseInMin', 'FirstResponseDiffInMin'], axis=1, inplace=True)
     excel.set_axis(['ticket_id', 'age', 'created', 'closed', 'first_block', 'first_repl', 'state', 'priority', 'queue', 'to_block', 'owner', 'first_name', 'last_name', 'company_id', 'customer_name', 'sender', 'subject', 'spent_time', 'message_tree', 'solution_in_min', 'solution_diff_min', 'first_response_in_min', 'first_response_diff_min'], axis=1, inplace=True)
+    excel.iloc[:, 1] = excel.iloc[:, 1].apply(match_time)
     return excel
 
 try:
     if len(sys.argv) < 2:
         raise SystemExit("Имя файла должно быть аргументом к скрипту")
-    # settings = pandas.read_json("conf.json")
     with open('conf.json', 'r') as f:
         data = json.load(f)
     engine = get_engine(data)
-    df = get_data_frame(data)
+    df = get_data_frame()
     if df.empty:
         engine.dispose()
         raise SystemExit("Некорректное имя excel файла, либо нет доступа")
