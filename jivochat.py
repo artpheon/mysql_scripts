5d4c2c2a96dc67f958f00c6d37b498cf1e64e8ea
hrobbin <idcdtokms@gmail.com>
2021-09-16 17:23:25 +0300

    added jivochat.py

--- jivochat.py
+++ jivochat.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from mysql.connector import Error
 from sqlalchemy import create_engine
+from sqlalchemy import insert
 import sys
 import re
 import json
@@ -9,6 +10,8 @@ import json
 # INSERT IGNORE
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.expression import Insert
+
+fine_cols = ['date', 'site_name', 'dial_acc', 'call_acc', 'dial_not_acc', 'call_not_acc', 'dial_noreply', 'offline_msg']
 
 @compiles(Insert)
 def _prefix_insert_with_ignore(insert, compiler, **kw):
@@ -24,8 +27,8 @@ def format_new_table(old):
     new = pd.DataFrame(old.iloc[start + 3:ends, :7])
     new['date'] = format_date(old.iloc[0, 0])
     new.insert(0, 'date', new.pop('date'))
-    new.reset_index(inplace=True)
-    new.columns = ['id', 'date', 'site_name', 'dial_acc', 'call_acc', 'dial_not_acc', 'call_not_acc', 'dial_noreply', 'offline_msg']
+    new.reset_index(drop=True, inplace=True)
+    new.columns = fine_cols
     return new
 
 def get_engine(data):
@@ -38,6 +41,18 @@ def get_engine(data):
     engine = create_engine('mysql+mysqlconnector://{0}:{1}@{2}:{3}'.format(user, password, host, port))
     return engine
 
+def create_statement(table, sql_table):
+    values = ""
+    for ind in table.index:
+        lst = [str(el) for el in table.iloc[ind].to_list()]
+        for i in range(2):
+            lst[i] = "\'" + lst[i] + "\'"
+        values += "({})".format(", ".join(lst))
+        if ind != table.last_valid_index():
+            values+=", "
+    values = re.sub('Мишлен', 'Michelin', values)
+    return "INSERT IGNORE INTO {}(".format(sql_table) + ', '.join(fine_cols) + ") VALUES {};".format(values)
+
 try:
     if len(sys.argv) < 2:
         raise SystemExit('Имя файла должно быть аргументом к скрипту')
@@ -47,16 +62,21 @@ try:
     if len(file.columns.to_list()) != 13:
         raise ValueError("В таблице неверное количество столбцов, должно быть 13")
     file.columns = ['x' + str(x) for x in range(13)]
-    new = format_new_table(file)
-    
+    table = format_new_table(file)
     engine = get_engine(data)
     
-    if new.empty:
+    if table.empty:
         engine.dispose()
         raise SystemExit("Некорректное имя excel файла, либо нет доступа")
     tname = data['schema']
     ttable = data['table_site_reports']
-    new.to_sql(name=ttable, schema=tname, con=engine, index=False, if_exists='append')
+
+    sql_table = "{}.{}".format(tname, ttable)
+    with engine.connect() as conn:
+        statement = create_statement(table, sql_table)
+        conn.execute(statement)
+        
+    # new.to_sql(name=ttable, schema=tname, con=engine, index=False, if_exists='append')
     print("Добавлены данные из {}".format(sys.argv[1]))
     engine.dispose()
 
